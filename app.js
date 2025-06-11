bfea62f27eac2369d50f5b1d0f46d1353964b2cf
DichOFC <akudankeluarga616@gmail.com>
2025-06-11 11:43:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const express = require("express");
-const cors = require("cors");
-const routes = require("./routes");
+import express from 'express';
+import cors from 'cors';
+import routes from './routes/index.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/', routes);
 
-app.use("/api", routes); // Endpoint semua dimulai dari /api
-
-module.exports = app;
+export default app;
